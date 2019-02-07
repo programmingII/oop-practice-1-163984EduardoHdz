9080dd0ffe50155c01a71c13d714a0dd7f51abc6
Eduardo Hernández <al163984@alumnos.uacj.mx>
2019-02-06 20:07:11 -0700

    Update Programa1Hello.java

--- Programa1Hello.java
+++ Programa1Hello.java
@@ -1,14 +1,9 @@
-/**
- *
- * @EduardO Hdz
- *Hora de inicio: 12:21
- *Hora que se termino: 12:22
+/*
+  @EduardO Hdz
+ Hora de inicio: 12:21
+ Hora que se termino: 12:22
  */
 public class Programa1Hello {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello!\n My name is: Eduardo Hdz\n");
