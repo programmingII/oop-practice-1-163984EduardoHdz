d33ae9a6124d266c0a69d7681b49ff51ac0e8d5f
Eduardo Hernández <al163984@alumnos.uacj.mx>
2019-02-24 12:52:00 -0700

    Add files via upload

---
+++ Programas/Programa1Hello/Programa1Hello.java
@@ -0,0 +1,12 @@
+/*
+  @EduardO Hdz
+ Hora de inicio: 12:21
+ Hora que se termino: 12:22
+ */
+public class Programa1Hello {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello!\n My name is: Eduardo Hdz\n");
+    }
+    
+}
---
+++ Programas/Programa2Suma/Programa2Suma.java
@@ -0,0 +1,15 @@
+/*
+  @EduardO Hdz
+ Hora de inicio: 19:41
+ Hora que se termino: 19:42
+ */
+public class Programa2Suma {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("La suma de 50+90 es ");
+        System.out.println(50+90);
+    }
+    
+}
+
+  
